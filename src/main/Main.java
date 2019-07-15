c0c199b2f182edeb4f0a430f467e1ccb3864b1c7
ShekhA01 <alexey.shekhter@googlemail.com>
2019-07-16 01:00:24 +0200

    one or multiple matches are beeing found

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,14 @@
 package main;
 
-import helper.Helper;
 import matcher.Match;
-
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //Scanner inputObject = new Scanner(System.in);
-        //System.out.print("Enter you phrase ");
-        //String inputPhrase = inputObject.nextLine();
-
-        //System.out.print("Enter the regular expression: ");
-        //String inputRegEx = inputObject.nextLine();
-
-        String inputPhrase = "bab bbb bcb";
-        String inputRegEx = "b.b";
+        String inputPhrase = "bab bbb bbb";
+        String inputRegEx = "bab";
         System.out.println("phrase: "+inputPhrase+", RegEx: "+inputRegEx);
 
-        System.out.println(Match.getMatch(Helper.cleanInput(inputPhrase), Helper.cleanInput(inputRegEx)));
+        Match.matching(inputPhrase,inputRegEx);
     }
 }
--- src/matcher/Match.java
+++ src/matcher/Match.java
@@ -1,49 +1,33 @@
 package matcher;
 
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.apache.commons.lang3.StringUtils.substringAfter;
-import static org.apache.commons.lang3.StringUtils.substringBefore;
 
 public class Match {
+    public static void matching(String phrase, String regEx){
+        List<Integer> matchingList = new ArrayList<>();
+        matchingList= firstIndexMatching(phrase, regEx);
 
-    public static List getMatch(String phrase, String regEx){
-        List<String> matchingOutputList = new ArrayList<>();
-
-        if (regEx.contains(".") && !regEx.contains("(")){
-            System.out.println("Regex has only .");
-            //expressionSubstring(regEx).compareToIgnoreCase(phrase);
-            String outputClean = expressionSubstring(regEx);
-            System.out.println(outputClean);
-
-//            Matcher matcher = Pattern.compile(regEx).matcher(phrase);
-//            while (matcher.find()) {
-//                matchingOutputList.add(matcher.group());
-//            }
-        }
-        else if (regEx.contains(".") && regEx.contains("(")){
-            System.out.println("Regex has . and ()");
-        }
-        else if (regEx.contains("(") && !regEx.contains(".")){
-            System.out.println("Regex contains only ()");
-        }
-        else {
-            System.out.println("Regex has no allowed operators");
+        for (int matchingListIndexPosition = 0; matchingListIndexPosition < matchingList.size(); matchingListIndexPosition++) {
+            System.out.print(phrase.substring(matchingList.get(matchingListIndexPosition), matchingList.get(matchingListIndexPosition)+regEx.length())+"; ");
         }
 
-        return matchingOutputList;
     }
 
-    public static String expressionSubstring(String expression){
-        if (expression.contains(".")){
-            String expressionNew = substringBefore(expression,".")+" "+substringAfter(expression, ".");
-            System.out.println(""+expressionNew);
-            return expressionNew;
+    public static List<Integer> firstIndexMatching(String phrase, String regEx){
+        List<Integer> matchingList = new ArrayList<>();
+
+        for (int letterInPhrase = 0; letterInPhrase < phrase.length(); letterInPhrase++) {
+            if (letterInPhrase+regEx.length()>phrase.length()){
+                break;
+            }
+            else if (phrase.substring(letterInPhrase,letterInPhrase+regEx.length()).equals(regEx)){
+                matchingList.add((letterInPhrase));
+            }
         }
 
-        return "Nothing to do";
+        return matchingList;
     }
+
 }

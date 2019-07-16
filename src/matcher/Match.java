93f00931d6cabe047afd938816e78d50060315dc
ShekhA01 <alexey.shekhter@googlemail.com>
2019-07-16 19:09:37 +0200

    full word recognition and "." are working "()" false display on group

--- src/matcher/Match.java
+++ src/matcher/Match.java
@@ -4,10 +4,11 @@ package matcher;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.apache.commons.lang3.StringUtils.*;
+
 public class Match {
     public static void matching(String phrase, String regEx){
-        List<Integer> matchingList = new ArrayList<>();
-        matchingList= firstIndexMatching(phrase, regEx);
+        List<Integer> matchingList = indexMatching(phrase, regEx);
 
         for (int matchingListIndexPosition = 0; matchingListIndexPosition < matchingList.size(); matchingListIndexPosition++) {
             System.out.print(phrase.substring(matchingList.get(matchingListIndexPosition), matchingList.get(matchingListIndexPosition)+regEx.length())+"; ");
@@ -15,7 +16,7 @@ public class Match {
 
     }
 
-    public static List<Integer> firstIndexMatching(String phrase, String regEx){
+    public static List<Integer> indexMatching(String phrase, String regEx){
         List<Integer> matchingList = new ArrayList<>();
 
         for (int letterInPhrase = 0; letterInPhrase < phrase.length(); letterInPhrase++) {
@@ -29,4 +30,27 @@ public class Match {
         return matchingList;
     }
 
+    public static int operandCounter(char operand, String regEx){
+        char[] phraseArray = regEx.toCharArray();
+        int counter=0;
+
+        for (Character wildcardOperator:phraseArray
+        ) {
+            if (wildcardOperator.equals(operand)){
+                counter++;
+            }
+        }
+        System.out.println("counter ist gleich: "+counter);
+
+        return counter;
+    }
+
+    public static int lengthOfNotEmptySide(String leftSide, String rightSide){
+        if (rightSide.isEmpty()){
+            return leftSide.length();
+        }
+        else {
+            return rightSide.length();
+        }
+    }
 }

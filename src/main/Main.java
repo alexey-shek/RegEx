48f12d266f6ac3a710de6968cdab1c3d1614878f
ShekhA01 <alexey.shekhter@googlemail.com>
2019-07-17 12:58:53 +0200

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,8 @@ import matcher.Match;
 public class Main {
 
     public static void main(String[] args) {
-        String inputPhrase = "bab bbb bbb";
-        String inputRegEx = "bab";
+        String inputPhrase = "baa bacb bcbd";
+        String inputRegEx = "b..d";
         System.out.println("phrase: "+inputPhrase+", RegEx: "+inputRegEx);
 
         Match.matching(inputPhrase,inputRegEx);
--- src/matcher/Match.java
+++ src/matcher/Match.java
@@ -19,14 +19,82 @@ public class Match {
     public static List<Integer> indexMatching(String phrase, String regEx){
         List<Integer> matchingList = new ArrayList<>();
 
-        for (int letterInPhrase = 0; letterInPhrase < phrase.length(); letterInPhrase++) {
-            if (letterInPhrase+regEx.length()>phrase.length()){
-                break;
-            }
-            else if (phrase.substring(letterInPhrase,letterInPhrase+regEx.length()).equals(regEx)){
-                matchingList.add((letterInPhrase));
+        if (phrase.contains(regEx)){
+            for (int indexInPhrase = 0; indexInPhrase < phrase.length(); indexInPhrase++) {
+                if (indexInPhrase+regEx.length()>phrase.length()){
+                    break;
+                }
+                else if (phrase.substring(indexInPhrase,indexInPhrase+regEx.length()).equals(regEx)){
+                    matchingList.add(indexInPhrase);
+                }
             }
         }
+        else if (regEx.contains(".") && !regEx.contains("(")){
+            System.out.println("Wildcard . found");
+            int counter = operandCounter('.',regEx);
+            String leftSideOfRegEx = substringBefore(regEx, ".");
+            String rightSideOfRegEx = substringAfter(regEx, ".").replaceAll("\\.","");
+            System.out.println("left side of regex "+leftSideOfRegEx);
+            System.out.println("right side of regex "+rightSideOfRegEx);
+
+
+            for (int indexInPhrase = 0; indexInPhrase < phrase.length()-lengthOfNotEmptySide(leftSideOfRegEx, rightSideOfRegEx)-counter; indexInPhrase++) {
+                System.out.println("right side is empty: "+rightSideOfRegEx.isEmpty()+" compare: "+phrase.substring(indexInPhrase, indexInPhrase+leftSideOfRegEx.length())+" to "+leftSideOfRegEx);
+
+                if (indexInPhrase+rightSideOfRegEx.length() > phrase.length()){
+                    break;
+                }
+                else if (phrase.substring(indexInPhrase,indexInPhrase+leftSideOfRegEx.length()).equals(leftSideOfRegEx) &&
+                        phrase.substring(indexInPhrase+leftSideOfRegEx.length()+counter,indexInPhrase+rightSideOfRegEx.length()+counter+1).equals(rightSideOfRegEx)){
+                    matchingList.add(indexInPhrase);
+                    System.out.println(matchingList);
+                }
+                else if (rightSideOfRegEx.isEmpty() && phrase.substring(indexInPhrase, indexInPhrase+leftSideOfRegEx.length()).equals(leftSideOfRegEx)){
+                    matchingList.add(indexInPhrase);
+                    System.out.println("right side is empty");
+                }
+                else if (leftSideOfRegEx.isEmpty() && phrase.substring(indexInPhrase, indexInPhrase+rightSideOfRegEx.length()+counter-1).equals(rightSideOfRegEx)){
+                    matchingList.add(indexInPhrase-1);
+                    System.out.println("left side is empty");
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+        else if (!regEx.contains(".") && regEx.contains("(")){
+            System.out.println("nur ( enthalten");
+            String afterBracket = substringAfter(regEx,"(");
+            String bracketContent = substringBefore(afterBracket,")");
+            System.out.println(bracketContent);
+
+            for (int indexInPhrase = 0; indexInPhrase < phrase.length(); indexInPhrase++) {
+                if (indexInPhrase+bracketContent.length() > phrase.length()){
+                    break;
+                }
+                else if (phrase.substring(indexInPhrase, indexInPhrase+bracketContent.length()).equals(bracketContent) ){
+                    System.out.println("compare: "+phrase.substring(indexInPhrase, indexInPhrase+bracketContent.length())+" and "+bracketContent);
+                    if (phrase.substring(indexInPhrase-bracketContent.length(), indexInPhrase-bracketContent.length()+1).equals(substringBefore(regEx, "(")) &&
+                            phrase.substring(indexInPhrase+bracketContent.length(), indexInPhrase+bracketContent.length()+1).equals(substringAfter(regEx, ")"))){
+                        matchingList.add(indexInPhrase);
+                        System.out.println(matchingList);
+                    }
+                    else {
+                        System.out.println("no match");
+
+                    }
+
+                }
+            }
+
+        }
+        else if (regEx.contains(".") && regEx.contains("(")){
+            System.out.println(". und ( sind enthalten");
+        }
+        else {
+            System.out.println("No matchings found!");
+        }
+
         return matchingList;
     }
 
